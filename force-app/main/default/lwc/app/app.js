a24dd1a9425fbd8c47096d4d11101f7f73eedb35
Rohan Maharjan <roharzan@gmail.com>
2025-11-08 21:44:53 +0545

    select your bike component

--- force-app/main/default/lwc/app/app.js
+++
@@ -1,8 +0,0 @@
-// app.js
-import { LightningElement } from 'lwc';
-export default class App extends LightningElement {
-  bike = {
-    name: 'Electra X4',
-    picture: 'https://s3-us-west-2.amazonaws.com/dev-or-devrl-s3-bucket/sample-apps/ebikes/electrax4.jpg'
-  };
-}
--- force-app/main/default/lwc/bike/bike.js
+++
@@ -1,5 +0,0 @@
-// bike.js
-import { LightningElement, api } from 'lwc';
-export default class Bike extends LightningElement {
-  @api bike;
-}
