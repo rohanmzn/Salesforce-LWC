9a3af73380a477473578124694d7e60e4668de1c
Rohan Maharjan <roharzan@gmail.com>
2025-11-09 12:34:12 +0545

    maximize counter and set prior

--- force-app/main/default/lwc/numerator/numerator.js
+++ force-app/main/default/lwc/numerator/numerator.js
@@ -1,7 +1,7 @@
 import { LightningElement, api} from 'lwc';
 
 export default class Numerator extends LightningElement {
-  @api counter = 0;
+  // @api counter = 0;
   handleIncrement() {
     this.counter++;
   }
@@ -11,5 +11,21 @@ export default class Numerator extends LightningElement {
   handleMultiply(event) {
     const factor = event.detail;
     this.counter *= factor;
+  }
+
+  @api
+  maximizeCounter() {
+    this.counter += 1000000;
+  }
+
+  _currentCount = 0;
+  priorCount = 0;
+  @api
+  get counter() {
+    return this._currentCount;
+  }
+  set counter(value) {
+    this.priorCount = this._currentCount;
+    this._currentCount = value;
   }
 }
